3cabe87245e69daa362b893393bb6e5fb65fda27
AnikIslam51 <anik.elahi@gmail.com>
2025-05-06 00:23:58 +0600

    New project 3

---
+++ project_3.py
@@ -0,0 +1,14 @@
+names = []
+ages = []
+emails = []
+
+for i in range(2):
+    name = input("Name: ")
+    age = input("Age: ")
+    email = input("Email: ")
+
+    names.append(name)
+    ages.append(age)
+    emails.append(email)
+
+print(names, ages, emails)
